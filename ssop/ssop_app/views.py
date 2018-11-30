40069f86fffef3a194751bb3efd174df2b14b31c
Santiago Faci <santiago.faci@gmail.com>
2018-11-30 17:44:22 +0100

    Added some files about the project

--- ssop/ssop_app/views.py
+++ ssop/ssop_app/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ssop_app/index.html')
+
+def registration(request):
+    return render(request, 'ssop_app/registration.html')
+
+def register_user(request):
+    pass
+
+
